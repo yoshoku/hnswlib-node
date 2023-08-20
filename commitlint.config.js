c7ad505ab8ee376e3ff4fd23a84736b1da7ecbca
yoshoku <yoshoku@outlook.com>
2023-08-20 21:19:09 +0900

    chore: ignore dependabot commit message

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,1 +1,4 @@
-module.exports = { extends: ['@commitlint/config-conventional'] };
+module.exports = {
+  extends: ['@commitlint/config-conventional'],
+  ignores: [(message) => /^chore\((deps|deps-dev)\): bump\ .+ from .+ to +./m.test(message)]
+};
