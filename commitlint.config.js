b6c18e9068359703306482bc36c00d9b7b3f7389
yoshoku <yoshoku@outlook.com>
2024-01-02 16:19:21 +0900

    chore: fix ignore rule

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,4 +1,4 @@
 module.exports = {
   extends: ['@commitlint/config-conventional'],
-  ignores: [(message) => /^chore\((deps|deps-dev)\): bump\ .+ from .+ to +./m.test(message)]
+  ignores: [(message) => /^chore\((deps|deps-dev)\):\s+bump\s+/m.test(message)]
 };
