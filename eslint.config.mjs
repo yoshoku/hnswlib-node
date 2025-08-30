cd4c5b6ce8bc7d5083bd98a9a43d6806d8083425
yoshoku <yoshoku@outlook.com>
2025-08-30 21:45:10 +0900

    chore: migrate to flat config

---
+++ eslint.config.mjs
@@ -0,0 +1,30 @@
+import js from '@eslint/js';
+import jest from 'eslint-plugin-jest';
+import jestExtended from 'eslint-plugin-jest-extended';
+
+export default [
+  {
+    ignores: ['node_modules/**', 'lib/**', 'build/**', 'doc/**']
+  },
+
+  js.configs.recommended,
+
+  {
+    files: ['test/**/*.js', 'commitlint.config.js'],
+    plugins: {
+      jest,
+      'jest-extended': jestExtended
+    },
+    languageOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'commonjs',
+      globals: {
+        ...jest.environments.globals.globals
+      }
+    },
+    rules: {
+      ...jest.configs.recommended.rules,
+      ...jestExtended.configs.all.rules
+    }
+  }
+];
